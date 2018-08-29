715696f168bb158ccbfdc0c3ea01026a7969e199
David Poole <pooleypoo@gmail.com>
2018-08-29 22:32:39 +1200

    Move MongoDB connection before routes

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const mongoose = require("mongoose");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 
+mongoose
+  .connect("mongodb://localhost/vidly")
+  .then(() => console.log("Connected to MongoDB/vidly..."))
+  .catch(err => console.error("Could not connect to MongoDB/vidly..."));
+
 app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
-
-mongoose
-  .connect("mongodb://localhost/vidly")
-  .then(() => console.log("Connected to MongoDB/vidly..."))
-  .catch(() => console.err("Could not connect to MongoDB/vidly...", err));
