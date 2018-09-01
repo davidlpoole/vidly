07dd9b01a9de3835030c9c4dc5c387faadc74a59
David Poole <14233215+davidlpoole@users.noreply.github.com>
2018-09-02 10:39:42 +1200

    basic register user function

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const genres = require('./routes/genres');
 const movies = require('./routes/movies');
 const customers = require('./routes/customers');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
 
 mongoose
   .connect('mongodb://localhost/vidly')
@@ -18,6 +19,7 @@ app.use('/api/genres', genres);
 app.use('/api/movies', movies);
 app.use('/api/customers', customers);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
