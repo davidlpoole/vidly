e39a51482075e7e8ab1106525fd21a15feb3ab2d
David Poole <14233215+davidlpoole@users.noreply.github.com>
2018-09-02 13:02:02 +1200

    squash! squash! adding user authentication

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,14 @@ const express = require('express');
 const router = express.Router();
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
+const Joi = require('joi');
 
 router.post('/', async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send('Invalid email or password.');
+  if (!user) return res.status(400).send('Invalid email or password.');
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send('Invalid email or password.');
@@ -31,7 +32,7 @@ function validate(req) {
       .required()
   };
 
-  return Joi.validate(user, schema);
+  return Joi.validate(req, schema);
 }
 
 module.exports = router;
